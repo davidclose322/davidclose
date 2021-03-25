08d343831f5a3175f75531f2631240e0001c0a35
dungod <80035347+dungod@users.noreply.github.com>
2021-03-25 17:51:12 +0800

    Add files via upload

---
+++ 3_design_pattern/Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends AbstractAnimal{
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		String animalName="นท";
+		super.color.paintAnimal(animalName);
+	}
+
+}
---
+++ 3_design_pattern/Yellow.java
@@ -0,0 +1,10 @@
+public class Yellow implements Color{
+
+	@Override
+	public void paintAnimal(String animalName) {
+		// TODO Auto-generated method stub
+		System.out.println("°Ñ"+animalName+"»­³É»ÆÉ«¡£");
+
+	}
+	
+}
