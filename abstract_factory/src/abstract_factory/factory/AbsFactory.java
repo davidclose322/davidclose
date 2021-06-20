be1f57fcdc73cac5868f5da847d73aaa6957795d
davidclose322 <957152803@qq.com>
2021-06-20 14:12:19 +0800

    del

--- abstract_factory/src/abstract_factory/factory/AbsFactory.java
+++
@@ -1,7 +0,0 @@
-package abstract_factory.factory;
-
-import abstract_factory.pizza.Pizza;
-
-public interface AbsFactory {
-    Pizza createPizza(String orderType);
-}
--- abstract_factory/src/abstract_factory/pizza/BJPepperPizza.java
+++
@@ -1,8 +0,0 @@
-package abstract_factory.pizza;
-
-public class BJPepperPizza extends Pizza{
-    public void setName(String name){
-        if (name.equals("BJPepperPizza"))
-            this.name = "北京的胡椒披萨";
-    }
-}
